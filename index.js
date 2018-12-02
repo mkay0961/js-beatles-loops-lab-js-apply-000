c40a0c858e6399ff262100b7986a3eeb28c56dfc
Matthew Kay <mkay0961@gmail.com>
2018-12-02 20:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(Musicians, Instruments){
+  var array = new Array();
+  for(var i = 0; i< Musicians.length; i++){
+    var msc = Musicians[i];
+    var inst = Instruments[i];
+    var string = msc + " plays " + inst;
+    array[i]= string;
+  }
+  
+  return array;
+}
+function johnLennonFacts(facts){
+  var string;
+  while(facts.length){
+    
+  }
+  
+  
+  
+  
+}
